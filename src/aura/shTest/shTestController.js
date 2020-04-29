4854bd462a3cff12e327dc1320ef98a84e2c619b
Seok Ho <lsh3715@gmail.com>
2020-04-29 14:11:52 +0900

    개발과제1 회원가입 UI 디자인1

--- src/aura/shTest/shTestController.js
+++ src/aura/shTest/shTestController.js
@@ -3,4 +3,26 @@
  */
 ({
 
+    Search: function (component, event, helper) {
+        console.clear();
+        console.log(component.find('v.reqData'));
+
+        console.log(component.get('v.labelMap'));
+
+        console.log(event.getSource().get("v.reqData"));
+
+/*        alert("입력한 이름: " + event.getSource().get("v.label"));*/
+    },
+
+    handleShowActiveSectionName: function (cmp, event, helper) {
+        alert(cmp.find("accordion").get('v.activeSectionName'));
+    },
+    handleSetActiveSectionC: function (cmp) {
+        cmp.find("accordion").set('v.activeSectionName', 'C');
+    },
+
+    isRegister: function (cmp, event, helper) {
+        alert("You clicked: " + event.getSource().get("v.label"));
+
+    }
 })
